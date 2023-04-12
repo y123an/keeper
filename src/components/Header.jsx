b7c099d41eaa89beade25d29ab3e4b42507bd6df
EBISHU <yohannesaregahabte@gmail.com>
2023-04-12 11:08:14 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import HighlightIcon from "@material-ui/icons/Highlight";
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
